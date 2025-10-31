67f7e0c3e88f678622df22eb789b08eab24ed3a2
Sathi <sathi@example.com>
2025-10-31 18:21:57 +0530

    adding react1

---
+++ react1/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello World from Sathish 👋</h1>
+      <p>Be the energy you want to attract.</p>
+    </div>
+  );
+}
+
+export default App;
